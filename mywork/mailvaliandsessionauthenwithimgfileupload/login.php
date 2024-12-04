979a5007de9f2aed0710bb73d0c0c1827b7226b6
shawon719 <sharmony.hashem2001@gmail.com>
2024-12-04 12:07:20 +0600

    add bootstrap in login page

--- mywork/mailvaliandsessionauthenwithimgfileupload/login.php
+++ mywork/mailvaliandsessionauthenwithimgfileupload/login.php
@@ -19,11 +19,50 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>log in page</title>
+
+    <link rel="stylesheet" href="dist/css/bootstrap.min.css">
 </head>
 <body>
     <?php
             echo isset($m)?$m:"";
     ?>
+        
+<nav class="navbar navbar-expand-lg navbar-light bg-danger">
+  <div class="container-fluid">
+    <a class="nav-link" href="#">HOME</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="">
+          <a class="nav-link" aria-current="page" href="#">REGISTRATION</a>
+        </li>
+        <li class="">
+          <a class="nav-link" href="#">DETAILS</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Log out 
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Home</a></li>
+            <li><a class="dropdown-item" href="#">Registration</a></li>
+            <li><hr class="dropdown-divider">Details</li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+        <!-- <li class="nav-item">
+          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+        </li> -->
+      </ul>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
 
     <form action="#" method="post">
         <h1>Please enter following field then log in student information</h1>
